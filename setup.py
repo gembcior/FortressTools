f2fe992a5f69aa453d0d3020d7764853af76e76b
gembcior <gembcior@gmail.com>
2020-03-11 20:24:25 +0100

    Python package structure organisation

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FortressTools",
+    version="0.0.1",
+    author="mywayof.dev",
+    author_email="mywayof.dev@gmail.com",
+    description="Personal Tools for development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://gogs.gembcior.pl/Gembcior/FortressTools.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OTHER",
+        "Operating System :: POSIX",
+        "Private :: Do Not Upload to public pypi server"
+    ],
+    python_requires='>=3.6',
+)
