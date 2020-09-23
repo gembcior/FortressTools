0a77ac51bdc7122fb70119f70236c058cbe18494
Gembcior <gembcior@gmail.com>
2020-09-23 22:44:16 +0200

    Delated timestamp from logger. For this purpose it is unneccessary.

--- modules/logger/logger.py
+++ modules/logger/logger.py
@@ -7,7 +7,7 @@ class FtLogger:
     def get_logger(name, verbose):
         logger = logging.getLogger(name)
         coloredlogs.DEFAULT_DATE_FORMAT = '%H:%M:%S'
-        coloredlogs.DEFAULT_LOG_FORMAT = '%(asctime)s [%(levelname)s] %(message)s'
+        coloredlogs.DEFAULT_LOG_FORMAT = '[%(levelname)s] %(message)s'
         coloredlogs.DEFAULT_FIELD_STYLES = {'asctime': {'color': 'black', 'bright': True}, 'levelname': {'bold': True, 'color': 'yellow'}}
         coloredlogs.install(level=verbose, logger=logger)
         return logger
