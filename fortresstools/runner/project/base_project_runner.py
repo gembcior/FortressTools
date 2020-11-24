d2bb818e167809b5848c77b3e1ebe143b3db2b40
Gembcior <gembcior@gmail.com>
2020-11-24 15:47:56 +0100

    Updated config file handling

--- fortresstools/runner/project/base_project_runner.py
+++ fortresstools/runner/project/base_project_runner.py
@@ -2,15 +2,16 @@ from ...parser.config.config_parser import ConfigParser
 import importlib.resources as resources
 
 class BaseProjectRunner:
-    def __init__(self):
+    def __init__(self, config_file="default.cfg"):
         self._settings = {}
         self._settings_object = None
         self._project_object = None
+        self._config_file = config_file
 
     @property
     def settings(self):
         self._settings["name"] = input("Project name: ")
-        with resources.path('config', 'default.cfg') as config:
+        with resources.path(__name__.split(".")[0] + '.config', self._config_file) as config:
             self._settings.update(ConfigParser.get_config(config, self._settings["project_type"]))
         return self._settings
 
